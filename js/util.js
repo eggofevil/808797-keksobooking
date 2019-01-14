15eca417531c1c7c5fbb86e2ff05206690b055b1
eggofevil <eggofevil@gmail.com>
2019-01-14 21:43:53 +0300

    удаляем shuffleArray и getRandomInteger

--- js/util.js
+++ js/util.js
@@ -2,25 +2,6 @@
 
 /* Модуль util.js */
 (function () {
-  var getRandomInteger = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  var shuffleArray = function (elements) {
-    if (elements.length <= 1) {
-      return elements;
-    }
-    elements = elements.slice();
-    var i = elements.length;
-    while (--i) {
-      var randomIndex = getRandomInteger(0, i);
-      var temp = elements[i];
-      elements[i] = elements[randomIndex];
-      elements[randomIndex] = temp;
-    }
-    return elements;
-  };
-
   var getRoundedElementWidth = function (container) {
     return parseInt(window.getComputedStyle(container).width, 10);
   };
@@ -35,8 +16,6 @@
   };
 
   window.util = {
-    getRandomInteger: getRandomInteger,
-    shuffleArray: shuffleArray,
     getRoundedElementWidth: getRoundedElementWidth,
     getParentElementWidth: getParentElementWidth,
     declineWord: declineWord
